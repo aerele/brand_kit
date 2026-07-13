4dec3d82638979ce1e6c83f8061faa3d51e00c86
Siddarth5100 <siddarthsiddhu5100@gmail.com>
2026-07-13 18:46:10 +0530

    feat: dynamically override Desk app icons using App UI settings

--- brand_kit/public/js/brand_kit.js
+++ brand_kit/public/js/brand_kit.js
@@ -4,7 +4,10 @@ frappe.utils.get_desktop_icon = function (icon_name, variant) {
     let app = frappe.boot.app_data?.find((app) => 
         app.app_title === icon_name || 
         app.app_name === icon_name ||
-        frappe.scrub(app.app_title) === frappe.scrub(icon_name)
+        frappe.scrub(app.app_title) === frappe.scrub(icon_name) ||
+        app.workspaces?.some(
+            workspace => frappe.scrub(workspace) === frappe.scrub(icon_name)
+        )
     );
 
     if (app?.app_logo_url) {
